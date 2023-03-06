68db98bdc62fe05aff39e417dcc6c68e0577f92f
Nikolay Yakimov <root@livid.pp.ru>
2023-03-06 17:05:04 +0300

    [Chore] Utility functions for byte sizes
    Problem: occasionally it's useful to get byte size of key/signature.
    
    Solution: export utilities for that.

--- src/Crypto/BLST/Internal/Types.hs
+++ src/Crypto/BLST/Internal/Types.hs
@@ -10,14 +10,30 @@ module Crypto.BLST.Internal.Types
 
 import Control.DeepSeq (NFData)
 import Data.Kind (Type)
+import Data.Proxy (Proxy(..))
+import GHC.TypeLits (KnownNat, Nat, natVal)
 
 import Crypto.BLST.Internal.Bindings.Types
-import Crypto.BLST.Internal.Classy (Curve, CurveToMsgPoint, CurveToPkPoint)
+import Crypto.BLST.Internal.Classy
+  (CompressedSize, Curve, CurveToMsgPoint, CurveToPkPoint, SerializedSize)
+
+-- | Data kind flag for 'ByteSize'.
+data SerializeOrCompress = Serialize | Compress
+
+type ByteSize :: SerializeOrCompress -> Type -> Nat
+-- | Size in bytes of serialized/compressed representations of basic types.
+type family ByteSize soc a
+
+-- | Convenience function to get byte size as an 'Int' value.
+byteSize :: forall soc a. KnownNat (ByteSize soc a) => Int
+byteSize = fromIntegral $ natVal @(ByteSize soc a) Proxy
 
 -- | Representation for the secret key.
 newtype SecretKey = SecretKey Scalar
   deriving stock (Eq, Show)
   deriving newtype NFData
+
+type instance ByteSize 'Serialize SecretKey = SkSerializeSize
 
 -- | Public key representation.
 type PublicKey :: Curve -> Type
@@ -25,8 +41,14 @@ newtype PublicKey c = PublicKey (Affine (CurveToPkPoint c))
   deriving stock (Eq, Show)
   deriving newtype NFData
 
+type instance ByteSize 'Serialize (PublicKey c) = SerializedSize (CurveToPkPoint c)
+type instance ByteSize 'Compress (PublicKey c) = CompressedSize (CurveToPkPoint c)
+
 -- | Signature representation.
 type Signature :: Curve -> EncodeMethod -> Type
 newtype Signature c m = Signature (Affine (CurveToMsgPoint c))
   deriving stock (Eq, Show)
   deriving newtype NFData
+
+type instance ByteSize 'Serialize (Signature c _) = SerializedSize (CurveToMsgPoint c)
+type instance ByteSize 'Compress (Signature c _) = CompressedSize (CurveToMsgPoint c)
